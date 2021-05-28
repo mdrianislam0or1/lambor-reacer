fea7cac562e6b5554880ed898b8a3ec6a67c8b82
RianIslam <rianislamdr@gmail.com>
2021-05-28 18:05:29 +0600

    add styled components and add section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
+import Section from './Section'
 
 const Home = () => {
     return (
-        <div>
-            
-        </div>
+        <Container>
+            home
+            <Section/>
+        </Container>
     )
 }
 
 export default Home
+
+
+const Container = styled.div`
+   height: 100vh;
+
+`
---
+++ src/components/Section.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Section = () => {
+    return (
+        <Wrap>
+            section
+        </Wrap>
+    )
+}
+
+export default Section
+
+const Wrap = styled.div`
+
+`
