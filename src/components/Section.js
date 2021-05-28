863368ad3a075c2a2cf5f9c766deb243c360d3ce
RianIslam <rianislamdr@gmail.com>
2021-05-28 18:41:40 +0600

    section styling

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,7 +4,21 @@ import styled from 'styled-components'
 const Section = () => {
     return (
         <Wrap>
-            section
+            <ItemText>
+                <h1>Model S</h1>
+                <p>Order Online for Touch delivery</p>
+            </ItemText>
+
+
+            <ButtonGroup>
+                <LeftButton>
+                    Custom Order
+                </LeftButton>
+
+                <RightButton>
+                    Existing Inventory
+                </RightButton>
+            </ButtonGroup>
         </Wrap>
     )
 }
@@ -12,5 +26,37 @@ const Section = () => {
 export default Section
 
 const Wrap = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: url("/img/lamborghiniOne.jpg");
+`
+
+const ItemText = styled.div`
+ padding-top: 15vh;
+ text-align: center;
+`
+
+const ButtonGroup = styled.div`
+
+`
+
+const LeftButton = styled.div`
+    background-color: rgba(23,26,32,0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius:100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size:12px;
+
+`
+const RightButton = styled(LeftButton)`
 
 `
